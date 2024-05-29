787555f6ae4c14658454ffc98165a733db36b60a
David Sorkin <sorkidavid@gmail.com>
2024-05-29 20:13:51 +0800

    more

---
+++ 4.MaxInt.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    a = 2147483647;
+    cout << "Int value: " << a << endl;
+    a = a + 1;
+    cout << "Int value after addition: " << a << endl;
+    return 0;
+}
+
+// int a = 10;
+// char group = 'A';
+// float price = 12.5sf;
