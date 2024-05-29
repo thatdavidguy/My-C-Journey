d401bc4b53dba1b1a417c89bea93753cb1e75281
David Sorkin <sorkidavid@gmail.com>
2024-05-29 19:37:11 +0800

    hello world

---
+++ 1.Helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// iostream is a library containing Input/Output stuff (like:cout)
+
+//for begusing namespace std; 
+
+int main(){
+    std::cout << "Hello World";
+    return 0;
+}
