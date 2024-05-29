c9f3c2090bf2729df8d15a9556db15078275d1b3
David Sorkin <sorkidavid@gmail.com>
2024-05-30 00:44:22 +0800

    loops done

---
+++ 11.For.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout << "Amount of A you want repeated" << endl;
+    cin >> a;
+    for (int i=0;i<a;i++){
+        cout << "A";
+    }
+    return 0;
+}
