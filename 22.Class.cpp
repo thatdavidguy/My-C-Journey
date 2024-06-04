83c52d84080e3df6d4253e0f111d8d0265709994
David Sorkin <sorkidavid@gmail.com>
2024-06-04 23:50:38 +0800

    data hiding in class

--- 22.Class.cpp
+++ 22.Class.cpp
@@ -3,10 +3,16 @@ using namespace std;
 
 
 class Rectangle{
-    public:
+    private:
         int x;
         int y;
-
+    public:
+        void setX(int xe){
+            x = xe;
+        }
+        void setY(int ye){
+            y = ye;
+        }
         int area(){
             return(x*y);
         }
@@ -18,9 +24,16 @@ class Rectangle{
 int main()
 {
     Rectangle r1;
-    r1.x = 10;
-    r1.y = 5;
+    r1.setX(10);
+    r1.setY(5);
     cout << r1.area() << endl;
-    cout << r1.circumference();
+    cout << r1.circumference()<<endl<<endl;
+    Rectangle r2;
+    Rectangle *ptr;
+    ptr=&r2;
+    ptr->setX(10);
+    ptr->setY(10);
+    cout << ptr->area() << endl;
+    cout << ptr->circumference()<<endl<<endl;
     return 0;
 }
